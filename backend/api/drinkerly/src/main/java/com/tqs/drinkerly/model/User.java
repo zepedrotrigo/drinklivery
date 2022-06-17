c3e4f79ee71c6a7469f65b36e54881b12c9aa2a8
zepedrotrigo <josetrigo2001@gmail.com>
2022-06-17 12:02:16 +0100

    deleted user class

--- backend/api/drinkerly/src/main/java/com/tqs/drinkerly/model/User.java
+++ backend/api/drinkerly/src/main/java/com/tqs/drinkerly/model/User.java
@@ -13,7 +13,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "User")
-public class User extends Person {
+public class User  {
+    private int age, nif;
+    private String firstName, lastName, password, address, phone, email;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -22,8 +25,93 @@ public class User extends Person {
     @OneToMany(mappedBy = "user")
     List<Request> requests;
 
-    public User(String firstName, String lastName, int age, int nif, String password, String address,
-            String phone, String email) {
-        super(firstName, lastName, age, nif, password, address, phone, email);
+    public User(String firstName, String lastName, String password, String address, int age, int nif, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.address = address;
+        this.age = age;
+        this.nif = nif;
+        this.phone = phone;
+        this.email = email;
     }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNif() {
+        return this.nif;
+    }
+
+    public void setNif(int nif) {
+        this.nif = nif;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            ", firstName='" + getFirstName() + "'" +
+            ", lastName='" + getLastName() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", address='" + getAddress() + "'" +
+            " age='" + getAge() + "'" +
+            ", nif='" + getNif() + "'" +
+            ", phone='" + getPhone() + "'" +
+            ", email='" + getEmail() + "'" +
+            "}";
+    }
+
 }
