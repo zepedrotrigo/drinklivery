5ec7df9f711e825843bb923f4c7cb247eda5befe
zepedrotrigo <josetrigo2001@gmail.com>
2022-06-18 15:47:03 +0100

    implemented register winery endpoint

--- backend/api/drinkerly/src/main/java/com/tqs/drinkerly/service/WineryService.java
+++ backend/api/drinkerly/src/main/java/com/tqs/drinkerly/service/WineryService.java
@@ -2,21 +2,29 @@ package com.tqs.drinkerly.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tqs.drinkerly.model.Winery;
 import com.tqs.drinkerly.repository.WineryRepository;
 
 @Service
 public class WineryService {
-    private final WineryRepository wineryRepository;
+    private static WineryRepository wineryRepository;
 
     @Autowired
     public WineryService(WineryRepository wineryRepository) {
-        this.wineryRepository = wineryRepository;
+        WineryService.wineryRepository = wineryRepository;
     }
 
-    public void save(Winery winery) {
+    public static void save(Winery winery) {
         wineryRepository.save(winery);
+    }
+
+    public static void registerWinery(@RequestParam String name, @RequestParam String address, @RequestParam int nif, @RequestParam String phone, @RequestParam String email, @RequestParam String website) {
+        
+        Winery winery = new Winery(name, address, nif, phone, email, website);
+        
+        save(winery);
     }
     
     public Iterable<Winery> getAllWinerys() {
