09f73bca5efba15baf59d036d6fbf6ab028ad48a
fmart8421 <fabio.m@ua.pt>
2022-06-21 02:31:26 +0100

    added user action and reducer

--- frontend/user/interface/src/redux/reducers/index.js
+++ frontend/user/interface/src/redux/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import loggedReducer from "./loggedReducer";
-import cartReducer from "./cartReducer"
+import cartReducer from "./cartReducer";
+import userReducer from "./userReducer";
 
 
 const rootReducer = combineReducers({
     isLogged:loggedReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    user:userReducer,
 })
 
 export default rootReducer;
